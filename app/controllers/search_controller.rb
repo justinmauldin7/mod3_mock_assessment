c97ab9b2267f3362e889495b5c37b50c27ad87b8
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-06 10:35:23 -0700

    Updates response query to NREL API to include all limitations on the search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,10 @@ class SearchController < ApplicationController
       faraday.adapter Faraday.default_adapter
     end
 
-  response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=DEMO_KEY&location=1617+Cole+Blvd+Golden+CO&fuel_type=ELEC&limit=10")
+  binding.pry
 
-  @station = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&location=#{params[:zip_code]}&radius=6.0&fuel_type=ELEC&fuel_type=LPG&limit=10")
+
+    @station = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
 end
