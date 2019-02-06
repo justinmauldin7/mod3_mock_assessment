4821402bd6aea322f0f9d14585f47216f99a5798
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-06 10:10:50 -0700

    Adds first attempt of getting API data in the search controller.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,12 @@
 class SearchController < ApplicationController
   def index
+    @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+
+  response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=DEMO_KEY&location=1617+Cole+Blvd+Golden+CO&fuel_type=ELEC&limit=10")
+
+  @station = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
 end
